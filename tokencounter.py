0da9f625310348fe5a2908581614c9a0e7c8ecf6
NickWithBotronics <122953474+NickWithBotronics@users.noreply.github.com>
2023-08-21 23:26:38 -0400

    Add files via upload

--- tokencounter.py
+++ tokencounter.py
@@ -1,15 +1,27 @@
 import re
 import argparse
+import chardet
 
 def count_tokens(filename):
-    with open(filename, 'r') as file:
+    with open(filename, 'rb') as file:
         data = file.read()
         
-    tokens = re.findall(r'\b\w+\b', data)
+    # Try to detect the encoding of the data
+    detected_encoding = chardet.detect(data)
+    encoding = detected_encoding.get('encoding', 'utf-8')
+
+    # Attempt to decode the data using the detected encoding
+    try:
+        text_data = data.decode(encoding)
+    except:
+        # Fallback to UTF-8 if decoding fails
+        text_data = data.decode('utf-8', errors='replace')
+        
+    tokens = re.findall(r'\b\w+\b', text_data)
     return len(tokens)
 
-parser = argparse.ArgumentParser(description="Count the tokens in a text file.")
-parser.add_argument('filename', type=str, help="Path to the text file.")
+parser = argparse.ArgumentParser(description="Count the tokens in a file.")
+parser.add_argument('filename', type=str, help="Path to the file.")
 
 args = parser.parse_args()
 
